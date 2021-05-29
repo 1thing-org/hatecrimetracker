d5982b5c00a58a09d654f69f768a74965beda746
Li Ma <lima@infolexllc.com>
2021-05-28 21:32:42 -0400

    Remove nodejs code

--- assets/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-const App = () => {
-  return (
-    <h1>Hate Crime Tracker (React on Sails.js)</h1>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
