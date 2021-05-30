7a3db19e06ab5c4686fb8a5bcc38b83722ba5b45
Li Ma <lima@infolexllc.com>
2021-05-29 22:26:52 -0400

    Fix run.sh to work for local postgresql

--- run.sh
+++ run.sh
@@ -2,9 +2,8 @@
 
 python3 -m venv env
 source env/bin/activate
-# pip install  -r requirements.txt
-# export DB_HOST=localhost:5432
-export CLOUD_SQL_CONNECTION_NAME=crimetracker
+pip install  -r requirements.txt
+export DB_HOST=localhost:5432
 export DB_USER=postgres
 export DB_PASS=postgres
 export DB_NAME=hatecrimetracker
