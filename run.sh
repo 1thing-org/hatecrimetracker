0cad44b9c80b9184776d9d324f43e869ff701c55
Li Ma <lima01@gmail.com>
2021-06-17 23:48:49 -0400

    Connect to the new postgres server

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 python3 -m venv env
 source env/bin/activate
 pip install  -r requirements.txt
-export DB_HOST=34.121.242.194:5432
+export DB_HOST=35.222.211.20:5432 
+# 34.121.242.194:5432
 export DB_USER=postgres
 export DB_PASS=postgres
 export DB_NAME=hatecrimetracker
