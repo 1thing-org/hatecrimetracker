a55f81b8285b1ee08826d947e742351285c2c917
Li Ma <lima01@gmail.com>
2021-08-07 17:05:45 -0400

    Do not clear cache if reasltime database update is not changed.

--- firestore/cachemanager.py
+++ firestore/cachemanager.py
@@ -7,15 +7,27 @@ ADMIN_CACHE = TTLCache(maxsize=128, ttl=3600*24)
 INCIDENT_CACHE = TTLCache(maxsize=1024, ttl=3600*24)
 INCIDENT_STATS_CACHE = TTLCache(maxsize=1024, ttl=3600*24)
 
+last_cache_update_date = ""
+
 def __clear_cache():
     ADMIN_CACHE.clear()
     INCIDENT_CACHE.clear()
     INCIDENT_STATS_CACHE.clear()
+
+
 def __listener(event):
+    global last_cache_update_date
+    if event.data == last_cache_update_date:
+        print("Cache update data not changed. Skip.")
+        return
+    last_cache_update_date = event.data
     __clear_cache()
-    print("cache_update event type:", event.event_type)  # can be 'put' or 'patch'
-    print("cache_update event path:", event.path)  # relative to the reference, it seems
-    print("cache_update event data:", event.data)  # new data at /reference/event.path. None if deleted
+    # can be 'put' or 'patch'
+    print("cache_update event type:", event.event_type)
+    # relative to the reference, it seems
+    print("cache_update event path:", event.path)
+    # new data at /reference/event.path. None if deleted
+    print("cache_update event data:", event.data)
 
 
 # Make sure to create a realtime db with the following URL and a json path called as /cache_update
@@ -25,6 +37,7 @@ options = {'databaseURL': 'https://hate-crime-tracker-default-rtdb.firebaseio.co
 filebase_app = firebase_admin.initialize_app(options=options, name=my_app_name)
 cache_update_db_ref = db.reference('/cache_update', app=filebase_app)
 cache_update_db_ref.listen(__listener)
+
 
 def flush_cache():
     __clear_cache()
