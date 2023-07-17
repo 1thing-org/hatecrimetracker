a8ed5b1a735e16914cc8ecc4f0661d40d2be259a
yy <yichen@YY-MacBook-Air.local>
2023-07-17 11:19:02 -0400

    code reformatted

--- incident_publisher.py
+++ incident_publisher.py
@@ -6,21 +6,25 @@ from firestore.incidents import Incident
 # check all incidents in database, and publish those have not yet been published yet
 # update incident.publish_status when done
 
+
 def publish_incidents():
     success = 0
     failed = 0
-    PUBLISHERS = {
-        'twitter': TwitterV2(),
-        'linkedin': LinkedIn()
-    }
-    incidents = Incident.collection.filter('incident_time', '>=', datetime(2022, 1, 22)).order('incident_time').fetch()
+    PUBLISHERS = {"twitter": TwitterV2(), "linkedin": LinkedIn()}
+    incidents = (
+        Incident.collection.filter("incident_time", ">=", datetime(2022, 1, 22))
+        .order("incident_time")
+        .fetch()
+    )
     for incident in incidents:
         for target, publisher in PUBLISHERS.items():
             if not incident.publish_status:
                 incident.publish_status = {}
             if target in incident.publish_status and incident.publish_status[target]:
                 continue  # already published to the target
-            print("Publishing incident to {} using publisher {}".format(target, publisher))
+            print(
+                "Publishing incident to {} using publisher {}".format(target, publisher)
+            )
             publish_time = publisher.publish(incident)
             if not publish_time:
                 print("Failed to publish to ", target)
@@ -31,4 +35,4 @@ def publish_incidents():
             # Uncomment me once the publishers are working
             incident.save()
             success += 1
-    print ("success:",success, " failed:", failed)
+    print("success:", success, " failed:", failed)
