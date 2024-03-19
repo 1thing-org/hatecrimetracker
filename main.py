48c0c091305f186816e83ffc2c6261513d07fc57
Ethan Yan <yancnov@qq.com>
2024-03-19 15:50:58 -0400

    fixed parent field issue (prod)

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ def _get_user(request) -> User:
 
 def _getCommonArgs():
     start = request.args.get(
-        "start", (date.fromisoformat("2022-11-01")).strftime("%Y-%m-%d")
+        "start", (datetime.now() - timedelta(days=90)).strftime("%Y-%m-%d")
     )
     end = request.args.get("end", datetime.now().strftime("%Y-%m-%d"))
     state = request.args.get("state", "")
@@ -134,7 +134,7 @@ def delete_incident(incident_id):
 
 @app.route("/incidents", methods=["POST"])
 def create_incident():
-    # _check_is_admin(request)
+    _check_is_admin(request)
     req = request.get_json().get("incident")
     if req is None:
         raise ValueError("Missing incident")
@@ -190,12 +190,12 @@ def get_stats():
 
 @app.route("/publish_incidents")
 def publish_incidents():
-    # header = request.headers.get("X-CloudScheduler", None)
-    # if not header:
-    #     raise ValueError(
-    #         "attempt to access cloud scheduler handler directly, "
-    #         "missing custom X-CloudScheduler header"
-    #     )
+    header = request.headers.get("X-CloudScheduler", None)
+    if not header:
+        raise ValueError(
+            "attempt to access cloud scheduler handler directly, "
+            "missing custom X-CloudScheduler header"
+        )
     result = incident_publisher.publish_incidents()
     return {"success": True, "result": result}
 
@@ -247,5 +247,4 @@ if __name__ == "__main__":
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
 
-    # app.run(host="127.0.0.1", port=8081, debug=True, threaded=True)
-    app.run(host="0.0.0.0", port=8082, debug=True)
+    app.run(host="127.0.0.1", port=8081, debug=True, threaded=True)
