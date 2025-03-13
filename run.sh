7ec706996c25045ea814b1dd85b3c828a5cb7132
JahongLiu <jahongliu@gmail.com>
2025-03-13 10:46:23 -0400

    add the way to set collection if running with run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@ python3.9 -m venv env
 source env/bin/activate
 pip install --upgrade pip
 pip install  -r requirements.txt
+# The default Firestore collection is incident, to use a different collection:
+# export FIRESTORE_COLLECTION=collection_name
 export GOOGLE_APPLICATION_CREDENTIALS=./hate-crime-tracker-7d52738f7212.json
 python main.py
