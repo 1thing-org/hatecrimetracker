5019e0c9db933ec7e5488fcd40e6bda0f9b08f4b
JahongLiu <jahongliu@gmail.com>
2025-06-25 10:08:57 -0400

    Switch to incident DB collection

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ source env/bin/activate
 pip install --upgrade pip
 pip install  -r requirements.txt
 # The default Firestore collection is incident, to use a different collection:
-export FIRESTORE_COLLECTION=incident-refined
-# the current collection: incident-refined
+export FIRESTORE_COLLECTION=incident
+# the current collection: incident
 export GOOGLE_APPLICATION_CREDENTIALS=./hate-crime-tracker-7d52738f7212.json
 python main.py
